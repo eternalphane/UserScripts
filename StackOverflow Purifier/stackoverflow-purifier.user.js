7725d69643f4a61a9a2aedec43a30067f537c83c
EternalPhane <eternalphane@gmail.com>
2018-12-09 05:58:58 -0500

    [stackoverflow-purifier] update include url

--- StackOverflow Purifier/stackoverflow-purifier.user.js
+++ StackOverflow Purifier/stackoverflow-purifier.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         StackOverflow净化器
 // @namespace    https://github.com/EternalPhane/UserScripts/
-// @version      0.2.1
+// @version      0.2.2
 // @description  屏蔽某个智障用户Ciro Santilli
 // @author       EternalPhane
-// @include      /^https?:\/\/stackoverflow\.com\//
+// @include      /^https?:\/\/(\w+\.)?stack(overflow|exchange)\.com\//
 // ==/UserScript==
 
 (() => {
