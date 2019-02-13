e7a38f79d6ab5d1b82ff902c1aa9617515897a06
Zongyuan Zuo <eternalphane@gmail.com>
2019-02-13 14:23:38 +0800

    bump version

--- StackOverflow Purifier/stackoverflow-purifier.user.js
+++ StackOverflow Purifier/stackoverflow-purifier.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         StackOverflow净化器
 // @namespace    https://github.com/EternalPhane/UserScripts/
-// @version      0.2.2
+// @version      0.2.3
 // @description  屏蔽某个智障用户Ciro Santilli
 // @author       EternalPhane
 // @include      /^https?:\/\/(\w+\.)?stack(overflow|exchange)\.com\//
