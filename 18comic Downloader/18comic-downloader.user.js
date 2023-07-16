d1d91d4ea2e5d6103b3e7b27f72a2ec095478ad3
Zuo Zongyuan <eternalphane@gmail.com>
2023-07-17 02:01:27 +0800

    [18comic] add support for mobile devices; bump version

--- 18comic Downloader/18comic-downloader.user.js
+++ 18comic Downloader/18comic-downloader.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         18comic漫画下载
-// @namespace    http://github.com/eternalphane/Userscripts/
-// @version      1.0.6
+// @namespace    https://github.com/eternalphane/Userscripts/
+// @version      1.0.7
 // @description  从18comic上下载cbz格式（整话阅读）或webp格式（分页阅读）的漫画
 // @author       eternalphane
 // @license      MIT
+// @downloadURL  https://github.com/eternalphane/UserScripts/raw/master/18comic%20Downloader/18comic-downloader.user.js
+// @supportURL   https://github.com/eternalphane/UserScripts/issues
 // @match        https://18comic.vip/photo/*
 // @match        https://18comic.org/photo/*
 // @match        https://jmcomic.me/photo/*
@@ -12,82 +14,64 @@
 // @match        https://jm-comic1.art/photo/*
 // @match        https://jm-comic2.art/photo/*
 // @match        https://jm-comic3.art/photo/*
-// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @require      https://unpkg.com/jszip@3.9.1/dist/jszip.min.js
-// @resource     css https://raw.githubusercontent.com/eternalphane/UserScripts/master/18comic%20Downloader/overlay.css
-// @resource     html https://raw.githubusercontent.com/eternalphane/UserScripts/master/18comic%20Downloader/overlay.html
-// @grant        GM_getResourceText
-// @grant        GM.getResourceText
-// @grant        GM_addStyle
-// @grant        GM.addStyle
 // ==/UserScript==
+'use strict';
 
-(async () => {
+setTimeout(async () => {
     const ICON_DOWNLOAD = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1ODAgNTIwIj48cGF0aCBkPSJNNTI4IDI4OXEyMCAwIDM0IDE0dDE0IDM0djEyOHEwIDIwLTE0IDM0dC0zNCAxNEg0OHEtMjAgMC0zNC0xNFQwIDQ2NVYzMzdxMC0yMCAxNC0zNHQzNC0xNGg5MmwtNDYtNDZxLTIyLTIzLTEwLTUyLjV0NDQtMjkuNWg2NFY0OXEwLTIwIDE0LTM0dDM0LTE0aDk2cTIwIDAgMzQgMTR0MTQgMzR2MTEyaDY0cTMyIDAgNDQuNSAyOS41VDQ4MiAyNDNsLTQ2IDQ2aDkyem0tNDAwLTgwbDE2MCAxNjAgMTYwLTE2MEgzMzZWNDloLTk2djE2MEgxMjh6bTQwMCAyNTZWMzM3SDM4OGwtNjYgNjZxLTE0IDE0LTM0IDE0dC0zNC0xNGwtNjYtNjZINDh2MTI4aDQ4MHptLTgxLTQ3cS03LTctNy0xN3Q3LTE3cTctNyAxNy03dDE3IDdxNyA3IDcgMTd0LTcgMTdxLTcgNy0xNyA3dC0xNy03eiIvPjwvc3ZnPg==';
-    const btns = [...document.querySelectorAll('li:has(.fa-bookmark)')].map(liPrev => {
+    const progress = new ProgressCircle;
+    progress.hidden = true;
+    document.body.appendChild(progress);
+    for (const liPrev of document.querySelectorAll('li:not(.ph-active):has(a .fa-bookmark), li.ph-active:has(a .fa-heart)')) {
         const li = document.createElement('li');
         for (const attr of liPrev.attributes) {
             li.setAttribute(attr.name, attr.value);
         }
         const btn = document.createElement('a');
         btn.href = '#';
-        btn.innerHTML = `
-<i class="far" style="background:url(${ICON_DOWNLOAD}) center / contain no-repeat">&#xfeff;</i>
-<span>下载</span>`;
-        li.appendChild(btn);
-        liPrev.after(li);
-        return btn;
-    });
-    GM.addStyle(await GM.getResourceText('css'));
-    const overlay = document.createElement('div');
-    overlay.id = 'dl-overlay';
-    overlay.innerHTML = await GM.getResourceText('html');
-    overlay.hidden = true;
-    document.body.appendChild(overlay);
-    const circle = overlay.querySelectorAll('circle')[1];
-    const number = overlay.querySelector('span');
-    const msg = overlay.querySelector('h2');
-    const updateProgress = (percent, text) => {
-        circle.style.strokeDasharray = `${percent} 100`;
-        number.innerText = Math.round(percent);
-        text != undefined && (msg.innerText = text);
-    };
-    btns.forEach(btn => btn.addEventListener('click', async e => {
-        e.preventDefault();
-        if (!overlay.hidden) {
-            return;
+        const styles = [`background:url(${ICON_DOWNLOAD}) center / contain no-repeat`];
+        if (liPrev.classList.contains('ph-active')) {
+            styles.push('filter:invert(1)');
         }
-        overlay.hidden = false;
-        document.body.classList.add('noscroll');
-        const selector = new URLSearchParams(location.search).get('read_mode') === 'read-by-page' ?
-            '.owl-item .center img' :
-            '.scramble-page img';
-        const pages = [...document.querySelectorAll(selector)];
-        const total = pages.length;
-        let progress = 0;
-        updateProgress(0, 'Downloading...');
-        const zip = new JSZip;
-        await Promise.all(pages.map(async page => {
-            try {
-                zip.file(...await download(page.dataset.original ?? page.dataset.src));
-            } catch {}
-            updateProgress(++progress * 100 / total);
-        }));
-        updateProgress(0, 'Compressing...');
-        // TODO: Select output format? (cbz, cbt, pdf)
-        save(
-            `${document.querySelector('.panel-heading .pull-left').textContent.trim()}.cbz`,
-            await zip.generateAsync({
+        btn.innerHTML = `
+<i class="far" style="${styles.join(';')}">&#xfeff;</i>
+<span>下载</span>`;
+        btn.addEventListener('click', async e => {
+            e.preventDefault();
+            btn.href = null;
+            progress.hidden = false;
+            progress.text = 'Downloading...';
+            const selector = new URLSearchParams(location.search).get('read_mode') === 'read-by-page' ?
+                '.owl-item .center img' :
+                '.scramble-page img';
+            const pages = [...document.querySelectorAll(selector)];
+            progress.max = pages.length;
+            const zip = new JSZip;
+            await Promise.all(pages.map(async page => {
+                try {
+                    zip.file(...await download(page.dataset.original ?? page.dataset.src));
+                } catch {}
+                ++progress.value;
+            }));
+            progress.text = 'Compressing...';
+            progress.value = 0;
+            progress.max = 100;
+            // TODO: Select output format? (cbz, cbt, pdf)
+            btn.download = `${document.querySelector('.panel-heading .pull-left').textContent.trim()}.cbz`
+            btn.href = URL.createObjectURL(await zip.generateAsync({
                 type: 'blob',
                 compression: 'DEFLATE',
                 compressionOptions: { level: 9 },
                 mimeType: 'application/vnd.comicbook+zip'
-            }, (meta) => updateProgress(meta.percent))
-        );
-        document.body.classList.remove('noscroll');
-        overlay.hidden = true;
-    }));
-})();
+            }, (meta) => progress.value = meta.percent));
+            progress.hidden = true;
+            setTimeout(() => btn.click(), 0);
+        }, { once: true });
+        li.appendChild(btn);
+        liPrev.after(li);
+    }
+}, 0);
 
 /**
  * @param {string} url
@@ -127,10 +111,172 @@ const download = async url => {
     })];
 };
 
-const save = (filename, blob) => {
-    const a = document.createElement('a');
-    a.href = URL.createObjectURL(blob);
-    a.download = filename;
-    a.click();
-    URL.revokeObjectURL(a.href);
-};
+const template = document.createElement('template');
+template.innerHTML = /* html */`
+<style>
+  #overlay {
+    position: fixed;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 999;
+    text-align: -moz-center;
+    text-align: -webkit-center;
+  }
+
+  #card {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 1.5em 4em;
+    border-radius: 0.5em;
+    background: white;
+  }
+
+  #progress {
+    position: relative;
+    width: 15em;
+    height: 15em;
+    border-radius: 50%;
+  }
+
+  #indicator {
+    transition: .5s;
+  }
+
+  #percentage {
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    font-size: 1.8em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%
+  }
+
+  #value {
+    font-size: 1.8em;
+  }
+</style>
+
+<div id="overlay">
+  <div id="card">
+    <div id="progress">
+      <svg viewBox="0 0 72 72" stroke="#cccccc" stroke-width="5" fill="none">
+        <circle cx="36" cy="36" r="32"/>
+        <circle id="indicator" cx="36" cy="36" r="32" stroke="#4cc790"/>
+      </svg>
+      <div id="percentage">
+        <p><span id="value"></span>%</p>
+      </div>
+    </div>
+    <h2 id="text"></h2>
+  </div>
+</div>
+`;
+
+class ProgressCircle extends HTMLElement {
+    static #attrs = /** @type {const} */ (['max', 'value', 'text', 'hidden']);
+
+    #isReady = false;
+    #isRenderScheduled = false;
+    #max = 1;
+    #value = 0;
+    #text = '';
+    /** @type {SVGCircleElement} */
+    #elIndicator;
+    /** @type {HTMLSpanElement} */
+    #elValue;
+    /** @type {HTMLHeadingElement} */
+    #elText;
+
+    static get observedAttributes() {
+        return this.#attrs;
+    }
+
+    get max() {
+        return this.#max;
+    }
+
+    set max(value) {
+        this.#max = value;
+        this.#render();
+    }
+
+    get value() {
+        return this.#value;
+    }
+
+    set value(value) {
+        this.#value = value;
+        this.#render();
+    }
+
+    get text() {
+        return this.#text;
+    }
+
+    set text(value) {
+        this.#text = value;
+        this.#elText.textContent = value;
+    }
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        if (!this.#isReady) {
+            this.#init();
+        }
+        this.#render();
+    }
+
+    /**
+     * @template {typeof ProgressCircle.observedAttributes[number]} P
+     * @param {P} name
+     * @param {ProgressCircle[P]} _oldValue
+     * @param {ProgressCircle[P]} newValue
+     */
+    attributeChangedCallback(name, _oldValue, newValue) {
+        if ('hidden' === name) {
+            newValue ?
+                document.body.style.removeProperty('overflow') :
+                document.body.style.setProperty('overflow', 'hidden', 'important');
+        } else {
+            this[name] = newValue;
+        }
+    }
+
+    #init() {
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.#elIndicator = this.shadowRoot.getElementById('indicator');
+        this.#elValue = this.shadowRoot.getElementById('value');
+        this.#elText = this.shadowRoot.getElementById('text');
+        this.#max = +(this.getAttribute('max') ?? this.#max);
+        this.#value = +(this.getAttribute('value') ?? this.#value);
+        this.text = this.getAttribute('text') ?? this.#text;
+        this.#isReady = true;
+    }
+
+    #render() {
+        if (!this.isConnected || !this.#isReady || this.hidden || this.#isRenderScheduled) {
+            return;
+        }
+        this.#isRenderScheduled = true;
+        requestAnimationFrame(() => {
+            this.#elIndicator.pathLength.baseVal = this.#max;
+            this.#elIndicator.style.strokeDasharray = this.#max;
+            this.#elIndicator.style.strokeDashoffset = this.#max - this.#value;
+            this.#elValue.textContent = Math.round(this.#value * 100 / this.#max);
+            this.#isRenderScheduled = false;
+        });
+    }
+}
+
+customElements.define('progress-circle', ProgressCircle);
